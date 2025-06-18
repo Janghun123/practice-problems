56befbaff4cc88bdeb18f6b899db609550788aeb
허훈 <ojim1025@gmail.com>
2025-06-18 12:32:48 +0900

    plus hash

--- prg.py
+++ prg.py
@@ -31,17 +31,21 @@ def print_all_students(students_list):
     
     print("[ 전체 학생 성적 ]")
     print("학번\t이름\t국어\t영어\t수학\t합계\t평균")
-
+    # number(학번)에 학번을 넣고 학번의 정보(infomation)를 삽입
     for number, info in students_list.items():
+        # 학번 출력 및 그 학번 내의 정보들을 출력
         print(f"{number}\t{info['이름']}\t{info['국어']}\t{info['영어']}\t{info['수학']}\t{info['합계']}\t{info['평균']:<.2f}")
-
+    # 특정 학번만 조회
 def search_student(students_list):
     track_student_number = int(input("조회할 학번 입력: "))
+    # 없을 경우 
     if track_student_number not in students_list:
         print("해당 학번의 학생 정보가 없습니다.")
         return
-    
+    # 해당 학번의 내용을 변수에 저장
     information = students_list[track_student_number]
+    # print(type(information))
+    # print(information)
     print("\n[ 학생 정보 ]")
     print(f"학번: {track_student_number}")
     print(f"이름: {information['이름']}")
@@ -50,7 +54,7 @@ def search_student(students_list):
     print(f"수학: {information['수학']}")
     print(f"합계: {information['합계']}")
     print(f"평균: {information['평균']:.2f}")
-    
+    # 특정 학번 삭제
 def delete_student(students_list):
     delete_student_number = int(input("삭제할 학번 입력: "))
     if delete_student_number not in students_list:
@@ -69,7 +73,6 @@ while True:
 4. 학생 성적 삭제
 5. 종료          """)
     choice_menu = input("메뉴 선택 (1 ~ 5): ")    
-    # 종료
     # 학생 성적 입력 학번 (정수), 이름(문자열), 국어/영어/수학 성적(정수) 입력
     # 합계와 평균 (소수점 2자리) 계산 및 저장
     if choice_menu == "1":
@@ -77,11 +80,13 @@ while True:
     # 이미 등록된 학번일 경우 저장하지 않고 오류 메세지 출력
     elif choice_menu == "2":
         print_all_students(students_list)
+    # 특정 학번 조회
     elif choice_menu == "3":
         search_student(students_list)
-        # 삭제 함수
+    # 삭제 함수
     elif choice_menu == "4":
         delete_student(students_list)
+    # 종료
     elif choice_menu == "5":
         print("프로그램을 종료합니다.")
         break
